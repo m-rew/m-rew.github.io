049ce34e9a2a79a784ff0fca27725668f9671ee5
m_rew <m_rew@vk.com>
2022-01-26 15:58:35 +0300

    Выпадающий список

---
+++ portfolio-frontend/geekbrains-shop/dist/js/form-select-dropdown.js
@@ -0,0 +1,29 @@
+let selectBtn1 = document.querySelector('#select__btn-1');
+let selectList1 = document.querySelector('#select__list-1');
+
+let selectBtn2 = document.querySelector('#select__btn-2');
+let selectList2 = document.querySelector('#select__list-2');
+
+let selectBtn3 = document.querySelector('#select__btn-3');
+let selectList3 = document.querySelector('#select__list-3');
+
+selectBtn1.addEventListener('click', function () {
+    selectList1.classList.toggle('select--active');
+
+    selectList2.classList.remove('select--active');
+    selectList3.classList.remove('select--active');
+});
+
+selectBtn2.addEventListener('click', function () {
+    selectList2.classList.toggle('select--active');
+
+    selectList1.classList.remove('select--active');
+    selectList3.classList.remove('select--active');
+});
+
+selectBtn3.addEventListener('click', function () {
+    selectList3.classList.toggle('select--active');
+
+    selectList1.classList.remove('select--active');
+    selectList2.classList.remove('select--active');
+});
