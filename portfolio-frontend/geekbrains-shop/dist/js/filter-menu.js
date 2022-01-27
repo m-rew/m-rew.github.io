c55fd57ad6fe52b49d12ef253a876288ff172b4f
m_rew <m_rew@vk.com>
2022-01-27 14:45:40 +0300

    Доработка catalog.html

--- portfolio-frontend/geekbrains-shop/dist/js/filter-menu.js
+++ portfolio-frontend/geekbrains-shop/dist/js/filter-menu.js
@@ -2,5 +2,9 @@ let filterBtn = document.querySelector('.filter__menu-title-wrap');
 let filterList = document.querySelector('.filter__menu-list-wrap');
 
 filterBtn.addEventListener('click', function () {
+    selectList1.classList.remove('select--active');
+    selectList2.classList.remove('select--active');
+    selectList3.classList.remove('select--active');
+
     filterList.classList.toggle('filter__menu-list-wrap--active');
 });
